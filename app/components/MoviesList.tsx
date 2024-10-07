ccadefc5a5480d5d3dd54cd80d00d29736cad44c
jujudev7 <penna.julien@gmail.com>
2024-10-07 16:42:58 +0200

    components/MoviesList.tsx added

---
+++ app/components/MoviesList.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import MovieCard from "./MovieCard";
+
+type Movie = {
+  title: string;
+  year: string;
+  poster: string;
+};
+
+type MoviesListProps = {
+  movies: Movie[];
+};
+
+const MoviesList: React.FC<MoviesListProps> = ({ movies }) => {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      {movies.map((movie, index) => (
+        <MovieCard
+          key={index}
+          title={movie.title}
+          year={movie.year}
+          poster={movie.poster}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default MoviesList;
