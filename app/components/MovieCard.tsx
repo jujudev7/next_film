2b24e1d2106955deda94f6c41de6f2dfa8326b77
jujudev7 <penna.julien@gmail.com>
2024-10-10 09:44:40 +0200

    sizes Image modified

--- app/components/MovieCard.tsx
+++ app/components/MovieCard.tsx
@@ -10,17 +10,18 @@ type MovieCardProps = {
 
 const MovieCard = ({ title, year, poster }: MovieCardProps) => {
   return (
-    <div className="border border-black p-2 mx-auto my-2 rounded-lg shadow-md bg-white hover:scale-105 hover:bg-rougevif hover:text-slate-100 overflow-hidden">
-      <Image
-        src={`/posters/${poster}`}
-        alt={`Affiche de ${title}`}
-        width={220}
-        height={330}
-        className="rounded-lg"
-        priority={true}
-      />
-      <div className="max-w-[220px] flex flex-row justify-between items-start mt-2">
-        {" "}
+    <div className="border border-black p-2 mx-auto my-2 rounded-lg shadow-md bg-white hover:scale-105 hover:bg-rougevif hover:text-slate-100 transition-all duration-300">
+      <div className="relative w-[220px] h-[330px]">
+        <Image
+          src={`/posters/${poster}`}
+          alt={`Affiche de ${title}`}
+          fill
+          sizes="(max-width: 768px) 100vw, 220px"
+          className="object-cover rounded-lg"
+          priority={true}
+        />
+      </div>
+      <div className="w-[220px] flex flex-row justify-between items-start mt-2">
         <h2 className="font-bold">{title}</h2>
         <p className="ml-1">{year}</p>
       </div>
