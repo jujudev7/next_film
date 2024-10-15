af5d2d4d027d6d2244f646b5ded0ce062f193989
jujudev7 <penna.julien@gmail.com>
2024-10-15 10:40:01 +0200

    button added in 404

--- app/404/page.tsx
+++ app/404/page.tsx
@@ -1,9 +1,14 @@
+import { Button } from "@/components/ui/button";
+
 export default function PageNotFoundError() {
   return (
-    <div class="flex items-center justify-center h-screen">
-      <h1 class="text-5xl text-white">
+    <div className="flex flex-col items-center justify-center min-h-[calc(100vh-200px)]">
+      <h1 className="text-5xl text-white mb-4">
         <span className="font-bold">404</span> | Oops, page non trouvée 🙃
       </h1>
+      <Button className="mt-6 text-lg p-8 bg-orange-500 rounded hover:bg-orange-600">
+        <a href="/">Retour à la page d&apos;accueil</a>
+      </Button>
     </div>
   );
 }
