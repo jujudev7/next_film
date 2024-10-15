73bfb3c79da9d41e067569a663bbc7491fcc2419
jujudev7 <penna.julien@gmail.com>
2024-10-15 11:43:27 +0200

    footer added & src logo corrected

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
+import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Link from "next/link";
 import Navbar from "./components/Navbar";
 import { MediaProvider } from "./context/MediaContext"; // Importation du contexte
 import "./globals.css";
-import type { Metadata } from "next";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,13 +32,23 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black`}
       >
-        <div className="max-w-[1200px] mx-auto pt-2">
-          <MediaProvider>
-            {" "}
-            {/* Enveloppe tout avec MediaProvider */}
-            <Navbar />
-            {children}
-          </MediaProvider>
+        <div className="min-h-screen flex flex-col">
+          {" "}
+          {/* Flex container pour l'écran complet */}
+          <div className="flex-grow">
+            <div className="max-w-[1200px] mx-auto pt-2">
+              <MediaProvider>
+                <Navbar />
+                {children} {/* Contenu principal, qui s'étend si nécessaire */}
+              </MediaProvider>
+            </div>
+          </div>
+          <footer className="flex justify-center text-slate-200 my-6">
+            Développé avec ❤️ par{" "}
+            <Link href="https://www.linkedin.com/in/developpeur-react-next-js-tailwind-frontend-julien-penna/">
+              <span className="hover:text-rougevif ml-1">Julien Penna</span>
+            </Link>
+          </footer>
         </div>
       </body>
     </html>
