ff986ee99db0806b2b0891154934f45a90d19e22
jujudev7 <penna.julien@gmail.com>
2024-10-15 10:53:20 +0200

    favicon added + export

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true, // Désactive l'optimisation d'image pour le mode exporté
+  },
+};
 
 export default nextConfig;
+
