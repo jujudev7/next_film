5efdea1b2d8081f39c21463e7091405ded9475a8
jujudev7 <penna.julien@gmail.com>
2024-10-29 16:31:12 +0100

    404 -> Not Found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,12 @@
 import { Button } from "@/components/ui/button";
 
-export default function PageNotFoundError() {
+export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center min-h-[calc(100vh-200px)]">
       <h1 className="text-5xl text-white mb-4">
         <span className="font-bold">404</span> | Oops, page non trouvée 🙃
       </h1>
-      <Button className="mt-6 text-lg p-8 bg-orange-500 rounded hover:bg-orange-600">
+      <Button className="mt-6 text-lg p-8 font-bold bg-orange-600 rounded hover:bg-orange-500 hover:text-black">
         <a href="/">Retour à la page d&apos;accueil</a>
       </Button>
     </div>
